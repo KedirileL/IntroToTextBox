4bbd27c0f43bda6cfeb8059d049eb0b38c63fc63
user <user@LAPTOP-KAN0NU4I>
2023-09-05 11:11:32 +0200

    Add project files.

---
+++ txt2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim stName As String
+        Dim stSurname As String
+        Dim stGender As String
+
+        stName = txtName.Text
+        stSurname = txtSurname.Text
+        stGender = txtGender.Text
+
+        MsgBox(" Hi " & stName & " " & stSurname & ". Its good to know you are " & stGender)
+    End Sub
+End Class
